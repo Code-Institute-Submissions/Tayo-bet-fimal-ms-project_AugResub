041d34c39bb539fbc1c89901c522461f8086b085
Tayo-bet <tayo4ayo@gmail.com>
2021-07-06 13:52:24 +0000

    corrections to search queries and product_details.html page

--- products/views.py
+++ products/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
+
+
 def all_products(request):
     """ View for individual product details """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Enter correct search word")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(descriptions__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
+
     }
+
     return render(request, 'products/products.html', context)
 
-def product_detail(request):
+
+def product_detail(request, product_id):
     """ All products, sorting and searching  View """
 
     product = get_object_or_404(Product, pk=product_id)
@@ -20,5 +39,5 @@ def product_detail(request):
     context = {
         'product': product
     }
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/products_detail.html', context)
 
