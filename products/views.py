433936d7c826c22ec9e27e69aee0878b92526dae
Tayo-bet <tayo4ayo@gmail.com>
2021-07-06 16:11:46 +0000

    category search

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -11,8 +11,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    Categories = None
 
     if request.GET:
+
+        if 'category' in request.GET:
+            Categories = request.GET['category']
+            products = products.filter(category=Categories)
+            
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,6 +31,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'store_categories': Category,
 
     }
 
