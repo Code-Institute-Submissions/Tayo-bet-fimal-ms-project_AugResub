05b030a15f8abc5f1e045fbcd17757602c19f0c4
Tayo-bet <tayo4ayo@gmail.com>
2021-07-10 22:48:56 +0000

    added subtotal calculation logic

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 def view_cart(request):
@@ -22,5 +22,62 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
 
     return redirect(redirect_url)
+
+def adjust_cart(request, item_id):
+    """ Logic to adjust and update items shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    inStock = None
+    if 'product.in_stock' in request.POST:
+        inStock = request.POST['product.in_stock']
+    cart = request.session.get('cart', {})
+
+    if inStock:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            del cart[item_id]
+            if not cart[item_id]:
+                cart.pop(item_id)
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['bag'] = cart
+    return redirect(reverse('view_cart'))
+
+def remove_from_cart(request, item_id):
+    """ Logic to remove and update items shopping cart """
+
+    try:
+       inStock = None
+       if 'product.in_stock' in request.POST:
+           inStock = request.POST['product.in_stock']
+       cart = request.session.get('cart', {})
+
+       if inStock:
+            del cart[item_id] 
+            if not cart[item_id]:
+               cart.pop(item_id)
+               
+       else:
+             cart.pop(item_id)
+
+
+       request.session['cart'] = cart
+       return HttpResponse(status=200)
+
+    except Exception as e:
+       return HttpResponse(status=500)
+      
+
+
+
+
+
+
+
 
 
