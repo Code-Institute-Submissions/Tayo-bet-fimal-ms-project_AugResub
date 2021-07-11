8426758359184109e9025912abb30e031156c72c
Tayo-bet <tayo4ayo@gmail.com>
2021-07-11 18:06:23 +0000

    notification css, cart additional messages and preview added

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -12,14 +12,15 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """ Logic to add items shopping cart """
-
-    product = Product.objects.get(pk=item_id)
+   
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {product.name} to your Cart')
@@ -28,9 +29,11 @@ def add_to_cart(request, item_id):
 
     return redirect(redirect_url)
 
+
 def adjust_cart(request, item_id):
     """ Logic to adjust and update items shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     inStock = None
     if 'product.in_stock' in request.POST:
@@ -44,19 +47,24 @@ def adjust_cart(request, item_id):
             del cart[item_id]
             if not cart[item_id]:
                 cart.pop(item_id)
+                messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         if quantity > 0:
             cart[item_id] = quantity
         else:
             cart.pop(item_id)
+            messages.success(request, f'Remove {product.name} from your Cart')
 
     request.session['bag'] = cart
     return redirect(reverse('view_cart'))
+
 
 def remove_from_cart(request, item_id):
     """ Logic to remove and update items shopping cart """
 
     try:
+       
+       product = get_object_or_404(Product, pk=item_id)
        inStock = None
        if 'product.in_stock' in request.POST:
            inStock = request.POST['product.in_stock']
@@ -66,15 +74,16 @@ def remove_from_cart(request, item_id):
             del cart[item_id] 
             if not cart[item_id]:
                cart.pop(item_id)
-               
+               messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
        else:
              cart.pop(item_id)
-
+             messages.success(request, f'Remove {product.name} from your Cart')
 
        request.session['cart'] = cart
        return HttpResponse(status=200)
 
     except Exception as e:
+       messages.error(request, f'Error removing item: {e}')
        return HttpResponse(status=500)
       
 
