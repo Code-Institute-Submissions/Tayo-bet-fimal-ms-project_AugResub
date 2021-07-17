787ea0390e482dc3576a3eb5c5d4184736b5a04a
Tayo-bet <tayo4ayo@gmail.com>
2021-07-17 10:38:59 +0000

    adding secured user to views

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
 from .forms import ProductForm
-
 
 
 def all_products(request):
@@ -70,11 +70,11 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
-
+@login_required
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, You are on the wrong Page: Admin Only!')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
@@ -96,10 +96,15 @@ def add_product(request):
     }
 
     return render(request, template, context)
+    
 
-
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, You are on the wrong Page: Admin Only')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -108,7 +113,7 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Error You form failed to Update, Try Again')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -122,10 +127,11 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, You are on the wrong Page: Admin Only!')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
