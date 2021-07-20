8224dcda3731374b08df773cc9ec5d2d49eb1b02
Tayo-bet <tayo4ayo@gmail.com>
2021-07-20 21:51:38 +0000

    corrections

--- checkout/forms.py
+++ checkout/forms.py
@@ -37,10 +37,3 @@ class OrderForm(forms.ModelForm):
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
-
-
-class CancelCheckoutOrder(forms.ModelForm):
-
-    class Meta:
-        model = cancel_checkout_order
-        fields = '__all__'
