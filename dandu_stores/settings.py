2470dceaeb12bfa56672ff01cfdaa8a201b66cfd
Tayo-bet <tayo4ayo@gmail.com>
2021-07-21 18:03:16 +0000

    deployment continue

--- dandu_stores/settings.py
+++ dandu_stores/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c1yet$0ewtdt9k!3)rk2h#37%+o8-k=zj5w0*))@v0_#e#+(91
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dandu-stores.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'dandu_stores.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
- DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
