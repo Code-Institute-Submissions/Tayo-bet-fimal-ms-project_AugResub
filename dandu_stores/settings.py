24b25230709c8ebe67b56d44a7c004031c2680cb
Tayo-bet <tayo4ayo@gmail.com>
2021-07-21 15:16:05 +0000

    Deployment: linking Herokupostress DB

--- dandu_stores/settings.py
+++ dandu_stores/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -118,13 +119,12 @@ WSGI_APPLICATION = 'dandu_stores.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+      'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
